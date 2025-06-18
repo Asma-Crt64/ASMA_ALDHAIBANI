1bd950730f959bfa398b613978c7ccbdb5af550c
ASMA <asmasaleh303@gmail.com>
2025-06-18 10:42:41 +0400

    Exercise8

---
+++ ex8.py
@@ -0,0 +1,16 @@
+def sum(num1, num2):
+    print(num1+num2)
+
+def sub(num1, num2):
+    print(num1-num2)
+
+def prod(num1, num2):
+    print(num1*num2)
+
+def div(num1, num2):
+    print(num1/num2)
+
+sum(5, 5)
+sub(6, 1)
+prod(2, 3)
+div(1, 1)
