12afae15ed270cfadbbaab24d63b81cab6e62c87
ASMA <asmasaleh303@gmail.com>
2025-06-18 10:37:32 +0400

    Exercise7

---
+++ ex7.py
@@ -0,0 +1,12 @@
+gradeNum=int(input("Enter your grade: "))
+
+if 100>=gradeNum>=90:
+    print("A")
+elif 90>gradeNum>=80:
+    print("B")
+elif 80>gradeNum>=70:
+    print("C")
+elif 70>gradeNum>=60:
+    print("D")
+else:
+    print("F")
